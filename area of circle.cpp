a37180cdc0a5182b1331202b8f9272b4a0891e78
MADANYADUVANSHI <msy941675@gmail.com>
2021-01-04 11:52:16 +0530

    first commit

---
+++ area of circle.cpp
@@ -0,0 +1,10 @@
+// find the area of circle 
+#include<stdio.h>
+int main()
+{
+	int r=5;
+	float pi=3.14;
+	int area=pi*r*r;
+	printf("the area of circle is %f",pi*r*r);
+	return 0;
+}
---
+++ fist prog. in c.cpp
@@ -0,0 +1,10 @@
+// find the sum of two numbers
+#include<stdio.h>
+int main()
+{
+	int a=6;
+	int b=8;
+	int c=a+b;
+	printf ("the value of c is %d",c=a+b);
+	return 0;
+}
